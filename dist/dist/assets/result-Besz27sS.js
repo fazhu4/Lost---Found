f280f84d37c063578cd01669729cb3e0c2b7b114
法助 <166514093+fazhu4@users.noreply.github.com>
2025-08-22 13:17:23 +0800

    Add files via upload

---
+++ dist/dist/assets/result-Besz27sS.js
@@ -0,0 +1,1 @@
+import{aJ as t,r}from"./index-D1X8d06w.js";const a=t("results",()=>{const e=r("");return{result:e,setResult:s=>{e.value=s},clearResult:()=>{e.value=void 0}}},{persist:!0});export{a as u};
