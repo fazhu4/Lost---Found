53196e4e80e83b62228a16071ad112479e7696b0
法助 <166514093+fazhu4@users.noreply.github.com>
2025-08-23 01:26:25 +0800

    Add files via upload

---
+++ dist/assets/result-BgW4fVVs.js
@@ -0,0 +1,1 @@
+import{aJ as t,r}from"./index-BvHV0dFE.js";const a=t("results",()=>{const e=r("");return{result:e,setResult:s=>{e.value=s},clearResult:()=>{e.value=void 0}}},{persist:!0});export{a as u};
